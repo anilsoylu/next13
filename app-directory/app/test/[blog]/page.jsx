619b75d572e7700a58693111958074641cae5298
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-04 03:36:48 +0300

    Create Page

---
+++ app-directory/app/test/[blog]/page.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function BlogPage(props) {
+  console.log(props)
+
+  return <div>Blog Page</div>
+}
+
+export default BlogPage
---
+++ app-directory/app/test/page.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function TestRoute() {
+  return <div>Test Route</div>
+}
+
+export default TestRoute
