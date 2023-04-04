249314d9b8e14a91d31b588194137c2b1a941842
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-04 03:47:05 +0300

    Loading State

---
+++ app-directory/app/test/loading.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function loading() {
+  return <h1>Test Loading</h1>
+}
+
+export default loading
--- app-directory/app/test/page.jsx
+++ app-directory/app/test/page.jsx
@@ -1,6 +1,11 @@
 import React from "react"
 
-function TestRoute() {
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function TestRoute() {
+  await delay(3000)
   return <div>Test Route</div>
 }
 
