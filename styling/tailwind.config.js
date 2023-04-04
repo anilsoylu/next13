92ad2200660ef9223c996cec65ab479fa1ae7fb5
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-04 04:53:03 +0300

    Tailwind & Flowbite

---
+++ styling/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/flowbite-react/**/*.js",
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+}
