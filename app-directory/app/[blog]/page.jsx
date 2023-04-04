5c36d130d36ae103a54704ab3499b738d79a09e3
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-04 04:11:13 +0300

    Title & Meta Tag

--- app-directory/app/[blog]/page.jsx
+++ app-directory/app/[blog]/page.jsx
@@ -1,6 +1,14 @@
 import { notFound } from "next/navigation"
 import React from "react"
 
+export async function generateMetadata({ params }) {
+  return {
+    title: "Blog Page",
+    description: "Blog Page Description",
+    keywords: "Blog Page Keywords",
+  }
+}
+
 function BlogPage({ searchParams }) {
   if (searchParams.test === "true") {
     notFound()
