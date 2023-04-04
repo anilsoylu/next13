c229506046a63644f7a50e28f8c3eca06f31a4da
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-04 03:17:58 +0300

    next/link

--- routing-navigation/app/page.jsx
+++ routing-navigation/app/page.jsx
@@ -1,5 +1,26 @@
+import Link from "next/link"
+
 function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <Link href="/about">About</Link>
+      <br />
+      <Link href="/docs">Docs</Link>
+      <br />
+      <Link href="/docs/getting-started">Docs/Getting Started</Link>
+      <br />
+      <Link
+        href={{
+          pathname: "/docs/getting-started",
+          query: { name: "Next.js", framework: "React" },
+        }}
+        target="_blank"
+        className="test"
+      >
+        About Page Obj
+      </Link>
+    </div>
+  )
 }
 
 export default HomePage
