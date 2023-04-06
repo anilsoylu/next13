0a65128f7b3bbba3df5b2fe3cf833bf57fe5e319
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-06 21:49:44 +0300

    server side rendering

---
+++ rendering-chapter/app/page.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Button from "../components/button"
+
+function HomePage() {
+  console.log("Home Page")
+
+  return (
+    <div>
+      Home Page
+      <Button />
+    </div>
+  )
+}
+
+export default HomePage
---
+++ rendering-chapter/components/button/index.js
@@ -0,0 +1,10 @@
+import "server-only"
+import React from "react"
+
+function button() {
+  console.log("button")
+
+  return <div>button</div>
+}
+
+export default button
