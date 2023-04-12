38de4f6336b5291a5038a66f3d120fcedfa0e84d
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-12 07:02:51 +0300

    nested server and client component structure

--- rendering-chapter/app/[id]/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function IdPage({ params }) {
-  console.log("IdPage:", params.id)
-
-  return <div>IdPage: {params.id}</div>
-}
-
-export default IdPage
--- rendering-chapter/app/about/page.js
+++ rendering-chapter/app/about/page.js
@@ -1,9 +1,18 @@
 import React from "react"
+import Input from "../../components/input"
+import Button from "../../components/button"
 
 function About() {
   console.log("About")
 
-  return <div>About</div>
+  return (
+    <div>
+      About
+      <Input>
+        <Button />
+      </Input>
+    </div>
+  )
 }
 
 export default About
