126e1fc29b01822217dcdccea21d20469e8082ab
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-12 06:49:17 +0300

    Static & Dynamic Rendering

---
+++ rendering-chapter/app/[id]/page.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function IdPage({ params }) {
+  console.log("IdPage:", params.id)
+
+  return <div>IdPage: {params.id}</div>
+}
+
+export default IdPage
---
+++ rendering-chapter/app/about/page.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function About() {
+  console.log("About")
+
+  return <div>About</div>
+}
+
+export default About
