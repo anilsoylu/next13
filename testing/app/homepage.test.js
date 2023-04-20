ab23434e4a40e8107c7a3370d8cf2ccab5bdce61
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-20 06:41:52 +0300

    input test - Type Event

--- testing/app/homepage.test.js
+++ testing/app/homepage.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Homepage from "./page"
 
 describe("Homepage", () => {
@@ -8,5 +9,14 @@ describe("Homepage", () => {
     expect(
       screen.getByRole("heading", { name: "Home Page" })
     ).toBeInTheDocument()
+  })
+
+  it("should update input value state", async () => {
+    render(<Homepage />)
+
+    const inputElement = screen.getByPlaceholderText(/search some/i)
+    await userEvent.type(inputElement, "Hey there")
+
+    expect(inputElement).toHaveValue("Hey there")
   })
 })
